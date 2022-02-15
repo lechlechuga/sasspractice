445d92c85d8c222b0436c015bd1fa728b4fd0a0a
lechlechuga <leslieechavez4@gmail.com>
2022-02-15 09:04:31 -0800

    updates

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,6 +3,7 @@ const hamburger = document.querySelector
 ('.menu-btn_burger');
 const nav = document.querySelector('nav');
 const menuNav = document.querySelector('menu-nav');
+const navItems = document.querySelectorAll('menu-nav_item')
 
 let showMenu = false;
 
@@ -13,6 +14,7 @@ function toggleMenu (){
         hamburger.classList.add('open');
         nav.classList.add('open');
         menuNav.classList.add('open');
+        navItems.forEach(item => item.classList.add('open'));
 
 
         showMenu = true;
@@ -20,6 +22,7 @@ function toggleMenu (){
         hamburger.classList.remove('open');
         nav.classList.remove('open');
         menuNav.classList.remove('open');
+        navItems.forEach(item => item.classList.remove('open'));
 
         showMenu = false;
     }
